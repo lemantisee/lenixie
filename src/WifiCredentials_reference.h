70413bfea7ea22d21f202a275aebd42f2ba3a570
Kostiantyn Chernenok <kos.chernenok@gmail.com>
2023-10-25 19:38:57 +0300

    [internal] Attach WifiCredentials_reference.h
    Signed-off-by: Kostiantyn Chernenok <kos.chernenok@gmail.com>

---
+++ src/WifiCredentials_reference.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace WifiCredentials
+{
+    const char *defaultApSsid = "lenixie";
+    const char *defaultApPassword = "lenixie1234";
+
+    const char *userSsid = "Your wifi network ssid";
+    const char *userPassword = "Your wifi network password";
+} // namespace name
