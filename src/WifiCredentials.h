60fc53828b44eb7bdac674577217344390909e64
Kostiantyn Chernenok <kos.chernenok@gmail.com>
2023-10-25 19:20:37 +0300

    [internal] some test with gitignore
    Signed-off-by: Kostiantyn Chernenok <kos.chernenok@gmail.com>

---
+++ src/WifiCredentials.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace WifiCredentials
+{
+    const char *defaultApSsid = "lenixie";
+    const char *defaultApPassword = "lenixie1234";
+
+    const char *userSsid = "le";
+    const char *userPassword = "Wemusttowifi14";
+} // namespace name
