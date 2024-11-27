d8109a7ecd76f22cd8cf59f30fa622c968aae7e2
Kostiantyn Chernenok <kos.chernenok@gmail.com>
2024-11-27 22:54:57 +0200

    [internal] Remove chain of responsibility from PanelClient

--- Firmware/PanelClient/PanelClient.cpp
+++ Firmware/PanelClient/PanelClient.cpp
@@ -1,30 +1,23 @@
 #include "PanelClient.h"
 
-PanelClient::PanelClient() 
-{
-    mLog.setClient(&mUsb);
-    mDateTime.setClient(&mUsb);
-    mNetwork.setClient(&mUsb);
-    mNtp.setClient(&mUsb);
+#include "RTClock.h"
+#include "ESP8266.h"
+#include "PanelMessage.h"
 
-    mLog.setNext(&mDateTime);
-    mDateTime.setNext(&mNetwork);
-    mNetwork.setNext(&mNtp);
-}
+#include "Logger.h"
 
-bool PanelClient::init(RTClock *clock, ESP8266 *wifi) 
+bool PanelClient::init(RTClock *clock, ESP8266 *wifi)
 {
-    mDateTime.setRtc(clock);
-    mNtp.setClock(clock);
-    mNetwork.setWifi(wifi);
-    
-    return mUsb.init(); 
+    mClock = clock;
+    mWifi = wifi;
+
+    return mUsb.init();
 }
 
 void PanelClient::process()
 {
     mUsb.process();
-    
+
     const SString<256> inBuffer = mUsb.popData();
     if (inBuffer.empty()) {
         return;
@@ -32,5 +25,196 @@ void PanelClient::process()
 
     const PanelMessage msg = PanelMessage::fromReport(inBuffer);
 
-    mLog.handle(msg);
+    switch (msg.getCmd()) {
+    case PanelMessage::GetLog: onLog(); break;
+
+    case PanelMessage::GetDateTime: onGetDateTime(); break;
+    case PanelMessage::SetDateTime: onSetDateTime(msg); break;
+
+    case PanelMessage::GetNetworkState: onNetworkState(); break;
+    case PanelMessage::ConnectToWifi: onNetworkConnect(msg); break;
+
+    case PanelMessage::GetNtpState: onNtpState(); break;
+    case PanelMessage::SyncNtpTime: onNtpSync(); break;
+    case PanelMessage::SetNtpServer: onSetServer(msg); break;
+    case PanelMessage::SetTimezone: onSetTimezone(msg); break;
+
+    default: LOG_ERROR("Unknown message: %i", msg.getCmd()); break;
+    }
+}
+
+void PanelClient::send(const SString<256> &report) { mUsb.sendData(report); }
+
+void PanelClient::onLog()
+{
+    if (Logger::empty()) {
+        sendLogEnd();
+        return;
+    }
+
+    SString<128> log = Logger::pop();
+    if (log.empty()) {
+        sendLogEnd();
+        return;
+    }
+
+    SString<256> escapedString = escapeString(log);
+
+    JsonObject j;
+
+    j.add("id", PanelMessage::LogUnit);
+    j.add("d", escapedString.c_str());
+
+    send(j.dump());
+}
+
+void PanelClient::sendLogEnd()
+{
+    JsonObject j;
+    j.add("id", PanelMessage::LogEnd);
+    send(j.dump());
+}
+
+SString<256> PanelClient::escapeString(const SString<128> &str) const
+{
+    SString<256> escString;
+
+    for (char c : str) {
+        if (c == 0) {
+            break;
+        }
+
+        if (c == '"') {
+            escString += '\\';
+        }
+
+        if (c > 31 && c < 127) {
+            escString += c;
+            continue;
+        }
+
+        const SString<256> hex = Logger::format("<0x%02X>", c);
+        escString.append(hex.c_str());
+    }
+
+    return escString;
+}
+
+void PanelClient::onGetDateTime()
+{
+    const DateTime &dateTime = mClock->getTime();
+    const SString<256> report = createDateTimeReport(dateTime);
+
+    if (report.empty()) {
+        LOG("Invalid time report");
+        return;
+    }
+
+    send(report);
+}
+
+void PanelClient::onSetDateTime(const PanelMessage &msg)
+{
+    DateTime dateTime;
+
+    dateTime.year = msg.getInt("y", 0);
+    dateTime.month = msg.getInt("mn", 0);
+    dateTime.monthDay = msg.getInt("md", 0);
+    dateTime.weekDay = msg.getInt("w", 0);
+
+    dateTime.hours = msg.getInt("h", 0);
+    dateTime.minutes = msg.getInt("m", 0);
+    dateTime.seconds = msg.getInt("s", 0);
+
+    mClock->setTime(dateTime);
+}
+
+SString<256> PanelClient::createDateTimeReport(const DateTime &dateTime) const
+{
+    JsonObject json;
+    json.add("id", PanelMessage::DateTimeState);
+    json.add("y", dateTime.year);
+    json.add("mn", dateTime.month);
+    json.add("md", dateTime.monthDay);
+
+    json.add("h", dateTime.hours);
+    json.add("m", dateTime.minutes);
+    json.add("s", dateTime.seconds);
+
+    return json.dump();
+}
+
+void PanelClient::onNetworkState()
+{
+    JsonObject json;
+    json.add("id", PanelMessage::NetworkState);
+
+    const bool connected = mWifi->isConnected();
+    json.add("s", connected ? 1 : 0);
+    if (connected) {
+        json.add("ssid", mWifi->getSsid().c_str());
+    }
+
+    send(json.dump());
+}
+
+void PanelClient::onNetworkConnect(const PanelMessage &msg)
+{
+    SString<256> ssid = msg.getString("s");
+    if (ssid.empty()) {
+        LOG_ERROR("Empty ssid");
+        return;
+    }
+
+    SString<256> pass = msg.getString("p");
+    if (pass.empty()) {
+        LOG_ERROR("Empty password");
+        return;
+    }
+
+    if (!mWifi->connectNetwork(ssid.c_str(), pass.c_str())) {
+        LOG_ERROR("Unable to connect to %s", ssid.c_str());
+        return;
+    }
+
+    LOG("Successfully connected to %s", ssid.c_str());
+}
+
+void PanelClient::onNtpState() 
+{
+    JsonObject json;
+    json.add("id", PanelMessage::NtpState);
+    json.add("url", mClock->getNtpServer().c_str());
+    json.add("t", mClock->getTimeZone());
+
+    send(json.dump());
+}
+
+void PanelClient::onNtpSync() { mClock->syncNtp(); }
+
+void PanelClient::onSetTimezone(const PanelMessage &msg) 
+{
+    int timezone = msg.getInt("t", 15);
+
+    if (timezone > 12 || timezone < -12) {
+        LOG_ERROR("Time zone value out of range: %i", timezone);
+        return;
+    }
+
+    LOG("Setting time zone: %i", timezone);
+
+    mClock->setTimeZone(int8_t(timezone));
+}
+
+void PanelClient::onSetServer(const PanelMessage &msg)
+{
+    SString<256> url = msg.getString("url");
+    if (url.empty()) {
+        LOG_ERROR("Empty ntp url");
+        return;
+    }
+
+    LOG("Setting ntp url: %s", url.c_str());
+
+    mClock->setNtpServer(SString<128>(url.data(), url.size()));
 }
--- Firmware/PanelClient/PanelClient.h
+++ Firmware/PanelClient/PanelClient.h
@@ -2,26 +2,40 @@
 
 #include "UsbDevice.h"
 
-#include "LogSession.h"
-#include "DateTimeSession.h"
-#include "NetworkSession.h"
-#include "NtpSession.h"
-
 class RTClock;
 class ESP8266;
+class DateTime;
+class PanelMessage;
 
 class PanelClient
 {
 public:
-    PanelClient();
+    PanelClient() = default;
     
     bool init(RTClock *clock, ESP8266 *wifi);
     void process();
 
 private:
+    void send(const SString<256> &report);
+
+    void onLog();
+    void sendLogEnd();
+    SString<256> escapeString(const SString<128> &str) const;
+
+    void onGetDateTime();
+    void onSetDateTime(const PanelMessage &msg);
+    SString<256> createDateTimeReport(const DateTime &dateTime) const;
+
+    void onNetworkState();
+    void onNetworkConnect(const PanelMessage &msg);
+
+    void onNtpState();
+    void onNtpSync();
+    void onSetTimezone(const PanelMessage &msg);
+    void onSetServer(const PanelMessage &msg);
+
+    RTClock *mClock = nullptr;
+    ESP8266 *mWifi = nullptr;
+
     UsbDevice mUsb;
-    LogSession mLog;
-    DateTimeSession mDateTime;
-    NetworkSession mNetwork;
-    NtpSession mNtp;
 };
